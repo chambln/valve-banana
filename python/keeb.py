f7158441f2e6cd6318120a2bab65b281671856e4
contrum <15685804+contrum@users.noreply.github.com>
2018-08-27 03:47:41 +0100

    Initial code

--- python/keeb.py
+++ python/keeb.py
@@ -2,102 +2,44 @@
 
 import low
 
+class Keymap(dict):
+    def __missing__(self, key):
+        '''Recursively instantiate Keymap objects to implement key 
+        modifiers.'''
+        self[key] = Keymap()
+        return self[key]
 
-class Keymap(object):
-    '''Defines key combinations bound to actions'''
-
-    def __init__(self, keymap={}):
-        self.keymap = keymap
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return str(self.keymap)
-
-    def __getitem__(self, key):
-        if key not in self.keymap:
-            # Fill out with dictionaries where needed
-            self.keymap[key] = {}
-        return self.keymap[key]
-
-    def __setitem__(self, key, value):
-        self.keymap[key] = value
-
-    def __delitem__(self, key):
-        del self.keymap[key]
-
-    def __iter__(self):
-        for group in self.keymap:
-            yield group
-
-    def map(self, *args):
-        '''Implement a complex key mapping into my keymap dictionary'''
-        m = Map(*args)
-        # Get the relevant modifier (just one for now)
-        mod = (*m.modifiers, 'none')[0]
-        # Add this mapping to myself
-        self[mod][m.final] = m.action
+    def map(self, combostr, action):
+        keys = combostr.split('-')
+        mod, final = keys[0], keys[1]
+        self[mod][final] = action
 
     def build(self):
-        pass
-
-
-class Map(object):
-    '''Links a key combination to an action'''
-
-    def __init__(self, keystring, action):
-        self.keycombo = Keycombo(keystring)
-        self.action = action
-
-    def __len__(self):
-        return len(self.modifiers) + 1
-
-    @property
-    def modifiers(self):
-        return self.keycombo.modifiers
-
-    @property
-    def final(self):
-        return self.keycombo.final
-
-
-class Keycombo(object):
-    '''Represents a combination of keys to be pressed simultaneously'''
-
-    def __init__(self, keystring):
-        self.keys = keystring.split('-')
-
-    def __repr__(self):
-        return "Keycombo('{}')".format('-'.join(self.keys))
-
-    @property
-    def modifiers(self):
-        return self.keys[:-1]
-
-    @property
-    def final(self):
-        return self.keys[-1]
-
+        master = low.Cfg(
+        
 
 def test_Keymap():
     k = Keymap()
-    k.map('w', '+forward')
-    k.map('enter', 'say')
-    k.map('shift-enter', 'say_team')
-    k.map('', 'exec sandbox')
-    #k.map('ctrl-shift-enter', 'gg')
+    k['enter'] = 'say'
+    k['shift']['enter'] = 'say_team'
     print('repr(k) ==', repr(k))
 
-def test_Map():
-    m = Map('ctrl-k', 'kill')
-    print('repr(m) ==', repr(m))
+def test_Keymap_map():
+    k = Keymap()
+    # Demonstrate that single modifiers work
+    k.map('alt-g', 'say gg')
+    k.map('alt-w', 'say ggwp')
+    k.map('shift-k', 'kill')
+    # Demonstrate that multiple modifiers do not work
+    k.map('ctrl-alt-g', 'say ggwp')
+    print("repr(k) ==", repr(k))
 
-def test_Keycombo():
-    kc = Keycombo('ctrl-shift-p')
-    print('repr(kc) ==', repr(kc))
-    print('repr(kc.modifiers) ==', repr(kc.modifiers))
-    print('repr(kc.final) ==', repr(kc.final))
+def test_Combo():
+    c = Combo('ctrl-shift-p')
+    print('repr(c) ==', repr(c))
+    print('repr(c.keys) ==', repr(c.keys))
+    print('repr(c.mods), repr(c.final) ==', repr(c.mods), repr(c.final))
+
 
 if __name__ == '__main__':
-    test_Keymap()
+    test_Keymap_map()
--- python/low.py
+++ python/low.py
@@ -3,154 +3,57 @@
 import os.path
 
 
-class Cfg(object):
-    '''Low level *.cfg file'''
-
-    def __init__(self, *commands):
-        self.commands = list(commands)
-
-    def __repr__(self):
-        return '<Cfg object with {} commands>'.format(len(self))
-
-    def __str__(self):
-        '''Return my commands with carriage returns after them'''
-        return ''.join([str(command)+'\n' for command in self.commands])
-
-    def __len__(self):
-        return len(self.commands)
-
-    def __iter__(foo, self):
-        for command in self.commands:
-            yield command
+class Cfg(list):
+    '''Subclass of list so that each element is a line of the file. Do 
+    Cfg().write(path) to export the file.'''
 
     def write(self, path):
-        # Use '.cfg' unless an extension was provided
+        '''Export as file to path. Uses .cfg if no extension is 
+        provided.'''
         root, ext = os.path.splitext(path)
-        if not ext:
-            path = root + '.cfg'
-        # Write to file
+        self.path = path if ext else root + '.cfg'
         with open(path, 'w') as cfg:
-            cfg.write(str(self))
-        # Print what just happened to help with debugging
-        print('Wrote {} commands to {}'.format(len(self), path))
-    
-    def append(self, command):
-        '''Append a command to this file'''
-        self.commands.append(command)
-
-    def set_generators(cls):
-        generators = ('Command', 'Bind', 'Unbind', 'Alias', 'Unalias', 
-                      'Comment', 'Exec', 'Cvar', 'Unbindall', 
-                      'BindToggle')
-        for gen in generators:
-            setattr(Cfg, gen.lower(), eval('lambda self, *args: self.append({}(*args))'.format(gen)))
+            for line in self:
+                cfg.write(line + '\n')
 
 
-class Command(object):
-    '''Represents a generic command that belongs to a *.cfg file'''
-
-    def __init__(self, *params):
-        self.params = params
-
-    def __repr__(self):
-        args = [repr(i) for i in self.params]
-        return 'Command({})'.format(', '.join(args))
-
-    def __len__(self):
-        return len(self.params)
+class Command(list):
+    _prefix = []
 
     def __str__(self):
-        '''
-        Return my low level line of code, surrounding only my last 
-        argument with quote marks. Single argument commands such as 
-        unbindall have no quotes.
-        '''
-        if len(self) > 1:
-            # Multiple argument command
-            args, last = self.params[:-1], self.params[-1]
-            return ' '.join(args) + ' "{}"'.format(last)
-        else:
-            # Single argument command, so no quotes
-            return self.params[0]
-
-
-class Comment(Command):
-    def __init__(self, comment):
-        super().__init__('// ', comment)
-
-    def __str__(self):
-        '''Override: don't use quotes for comments'''
-        return '{}{}'.format(*self.params)
-
-
-class Exec(Command):
-    def __init__(self, path):
-        super().__init__('exec', path)
-
-
-class Cvar(Command):
-    def __init__(self, cvar, value):
-        super().__init__(cvar, value)
+        last = '"{}"'.format(self.pop())
+        return ' '.join(self._prefix + self + [last])
 
 
 class Alias(Command):
-    def __init__(self, alias, cmd):
-        super().__init__('alias', alias, cmd)
-
-
-class Unalias(Alias):
-    def __init__(self, alias):
-        super().__init__(alias, '')
+    _prefix = ['alias',]
 
 
 class Bind(Command):
-    def __init__(self, key, cmd):
-        super().__init__('bind', key, cmd)
+    _prefix = ['bind',]
 
 
 class Unbind(Command):
-    def __init__(self, key):
-        super().__init__('unbind', key)
+    _prefix = ['unbind',]
 
-
-class Unbindall(Command):
-    def __init__(self):
-        super().__init__('unbindall')
-
-    def __str__(self):
-        '''Override: don't use quotes at all'''
-        return '{}'.format(*self.params)
-
-
-class BindToggle(Command):
-    def __init__(self, key, cvar):
-        super().__init__('BindToggle', key, cvar)
-
-
-def test_Command():
-    cmd = Command('bind', 'enter', 'say')
-    print('repr(cmd) ==', repr(repr(cmd)))
-    print(' str(cmd) ==', repr( str(cmd)))
-
-def test_Cfg():
-    cfg = Cfg('bind enter say', 'bind space +jump')
-    print('repr(cfg) ==', repr(repr(cfg)))
-    print(' str(cfg) ==', repr( str(cfg)))
-    cfg.write('foo')
 
 def test():
-    cfg = Cfg()
-    cfg.set_generators()
-    cfg.cvar('hud_scaling', '0.90')
-    cfg.bind('w', '+forward')
-    cfg.bind('d', '+back')
-    cfg.unbindall()
-    cfg.alias('foo', 'bar')
-    cfg.exec('mode/sandbox.cfg')
-    cfg.comment('TODO: ALL THE THINGS')
-    print('repr(cfg) ==', repr(repr(cfg)))
-    cfg.write('test')
+    foo = Command(('foo',))
+    print(foo)
+
+    c = Command(('foo', 'bar', 'baz', 'qux'))
+    print(c)
+
+    a = Alias(('+info', 'cl_showscores 1'))
+    print(a)
+
+    b = Bind(('f', '+moveright'))
+    print(b)
+
+    u = Unbind(('enter',))
+    print(u)
+
+    hs = Command(('hud_scaling', '0.90'))
 
 if __name__ == '__main__':
     test()
-    pass
