c85e394f37a1baeb99f3b551ec2e1d7acb4b7298
contrum <15685804+contrum@users.noreply.github.com>
2018-08-27 05:12:52 +0100

    Writing

--- python/keeb.py
+++ python/keeb.py
@@ -1,1 +1,66 @@
 # keeb.py
+
+import low
+
+
+class Keymap(dict):
+    def __missing__(self, key):
+        self[key] = {}
+        return self[key]
+
+    def map(self, combo, value):
+        keys = combo.split('-')
+        final = keys[-1]
+        if len(keys) == 1:
+            # No modifier keys
+            mod = ''
+        else:
+            # Just take first of modifiers given
+            mod = keys[0]
+        self[mod][final] = value
+
+    def build_(self):
+        # First, handle master bindings if any
+        if '' in self:
+            binds = [low.Command(('bind', key, val))
+                     for key, val in self[''].items()]
+            master = low.Cfg(binds)
+            master.write('master')
+            self.master = self['']
+            del self['']
+        # Then handle any modified bindings
+        for mod, mapdict in self.items():
+            print(mod, mapdict)
+            up = low.Cfg([low.Command(('bind', key, self.master[key])) 
+                          for key in mapdict])
+            dn = low.Cfg([low.Command(('bind', key, val))
+                          for key, val in mapdict.items()])
+            dn.write(mod + '_dn')
+            up.write(mod + '_up')
+
+    def build(self):
+        # Isolate the master bindings
+        self.master = self.pop('')
+        # Handle modified bindings
+        for mod, mapdict in self.items():
+            dn = low.Cfg([
+                low.Bind(key, val)
+                for key, val in mapdict.items()
+            ])
+            up = low.Cfg([
+                low.Bind(key, self.master[key])
+                for key in mapdict
+            ])
+            dn.write(mod + '_dn')
+            up.write(mod + '_up')
+        # Handle master bindings and aliases
+
+
+def test():
+    k = Keymap()
+    k.map('enter', 'say')
+    k.map('shift-enter', 'say_team')
+    k.build()
+
+if __name__ == '__main__':
+    test()
--- python/low.py
+++ python/low.py
@@ -10,4 +10,67 @@ class Cfg(list):
             path = root + '.cfg'
         with open(path, 'w') as cfg:
             for line in self:
-                cfg.write(line + '\n')
+                cfg.write(str(line) + '\n')
+
+
+class Command(list):
+    '''Generic command. Also used for cvar commands since they have no prefix.'''
+    def __init__(self, *args):
+        list.__init__(self, *args)
+
+    def __str__(self):
+        if len(self) == 1:
+            # Don't use quotes for singleton commands like unbindall
+            return self[0]
+        else:
+            # Quote only the final argument
+            final = '"{}"'.format(self[-1])
+            return ' '.join(self[:-1] + [final])
+
+
+class Echo(Command):
+    def __init__(self, msg):
+        super().__init__(('echo', msg))
+        
+class Exec(Command):
+    def __init__(self, path):
+        super().__init__(('exec', path))
+        
+class Alias(Command):
+    def __init__(self, alias, cmd):
+        super().__init__(('alias', alias, cmd))
+        
+class Bind(Command):
+    def __init__(self, key, cmd):
+        super().__init__(('bind', key, cmd))
+
+class Unbind(Command):
+    def __init__(self, key):
+        super().__init__(('unbind', key))
+
+class Unbindall(Command):
+    def __init__(self):
+        super().__init__(('unbindall',))
+        
+
+def test_bind():
+    b = Bind('q', 'drop')
+    print(b)
+    u = Unbind('r')
+    print(u)
+    e = Echo('foo')
+    print(e)
+
+def test_command():
+    c = Command(('unbindall',))
+    print(c)
+    d = Command(('bind', 'enter', 'say'))
+    print(d)
+
+def test_cfg():
+    c = Cfg([Command(('foo', 'bar'))])
+    print(c)
+    c.write('foo')
+
+if __name__ == '__main__':
+    test_bind()
