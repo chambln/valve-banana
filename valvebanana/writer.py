8684b6f8d1268d2767cbd80d67b06add7afb03f0
Greg Chamberlain <cosineblue@gmail.com>
2019-04-10 08:27:48 +0100

    Put writer functions into an object environment.

--- valvebanana/writer.py
+++ valvebanana/writer.py
@@ -3,48 +3,65 @@
 # Constants
 PREFIX = '_vb'
 
-
 # Utitlity functions
 quote = lambda x: '"{}"'.format(x)
 digest = lambda x: PREFIX + str(abs(hash(str(x))))[:3]
 
 
-# Writer functions
+class Env(object):
+    def __init__(self, bib=[]):
+        self.bib = bib
 
-par = lambda ss: '; '.join(sen(s) for s in ss)
-sen = lambda ts: ' '.join(term(t) for t in ts)
-term = lambda x: x if isinstance(x, str) else quote(subpar(x))
-subpar = lambda ss: '; '.join(subsen(s) for s in ss)
-subsen = lambda ts: ' '.join(subterm(t) for t in ts)
-subterm = lambda x: x if isinstance(x, str) else ref(x)
+    def par(self, ss):
+        return '; '.join(self.sen(s) for s in ss)
 
-def ref(x):
-    d = digest(x)
-    bib = ('alias', d, x)
-    print('Bib:', sen(bib))
-    return d
+    def sen(self, ts):
+        return ' '.join(self.term(t) for t in ts)
+
+    def term(self, x):
+        return x if isinstance(x, str) else quote(self.subpar(x))
+
+    def subpar(self, ss):
+        return '; '.join(self.subsen(s) for s in ss)
+
+    def subsen(self, ts):
+        return ' '.join(self.subterm(t) for t in ts)
+
+    def subterm(self, x):
+        return x if isinstance(x, str) else self.ref(x)
+
+    def ref(self, x):
+        d = digest(x)
+        reference = ('alias', d, x)
+        self.bib.append(self.sen(reference))
+        return d
 
 # Tests
 if __name__ == '__main__':
     # Test 1: complex sentence with no references.
+    env = Env()
+    print('\n** TEST 1 **')
     p = [('bind', 'tab', [
         ('+showscores',),
         ('net_graphtext', '1'),
         ('cl_showpos', '1'),
     ]),]
-    print(par(p))
+    print(env.par(p))
     # -> bind tab "+showscores; net_graphtext 1; cl_showpos 1"
 
     # Test 2: complex sentence: complex sentence with
     # references.
+    print('\n** TEST 2 **')
+    env = Env()
     rebinds = [
         ('bind', 't', [('buy', 'awp')]),
         (
             'bind',
             'space',
-            [('buy', 'kevlar'), ('cl_crosshaircolor', '0'),
-            (p,)]
+            [('buy', 'kevlar'), ('cl_crosshaircolor', '0')],
         )
     ]
     p = [('bind', 'mouse3', rebinds),]
-    print(par(p))
+    print(env.par(p))
+    for i in env.bib:
+        print(i)
